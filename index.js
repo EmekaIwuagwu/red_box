198c73d2fd6c223757924ef68299190d2e0b9200
EmekaIwuagwu <iwuagwuemmy@yahoo.com>
2024-10-27 09:36:10 +0100

    implemented changes and designs

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Endpoint to receive form data
