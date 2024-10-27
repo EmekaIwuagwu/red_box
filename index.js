601853c33ae90c01d25bb48e5613cfb7ba056c46
EmekaIwuagwu <iwuagwuemmy@yahoo.com>
2024-10-27 09:30:07 +0100

    implemented changes and designs

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 // Endpoint to receive form data
